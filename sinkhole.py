aaa90fc75dfdf34fa6cabe301568e8ea9052974b
Sam0x90 <samuel.rossier@outlook.com>
2023-05-19 23:13:22 +0400

    Update sinkhole.py
    Updating the comments

--- sinkhole.py
+++ sinkhole.py
@@ -6,27 +6,29 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("WebServer")
 
-# Handler class for serving landing page and handling the redirection 
+# Handler class for serving landing page
 class WebHandler(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
-                #S erve the landing page for HTTP requests
+                #Serve the landing page for HTTP requests
                 self.send_response(http.HTTPStatus.OK)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
+                
                 # Open and read HTML file
                 with open('landing_sink.html', 'r') as file:
                         html_content = file.read()
+                
                 # Send the HTML content as the response
                 self.wfile.write(html_content.encode('utf-8'))
 
-# Create a socket server with both HTTP and HTTPS support
+# Create a socket server for HTTP port
 http_server = socketserver.TCPServer(('', 80), WebHandler)
 http_server.allow_reuse_address = True
-logger.info("HTTP server started on port 80")
 
-#Start both servers simultaneously
+#Start server
 try:
         http_server.serve_forever()
+        logger.info("HTTP server started on port 80")
 
 except KeyboardInterrupt:
         pass
