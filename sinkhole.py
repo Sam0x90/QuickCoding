3fd8786405d06b48d720d27833a2764f256d96a6
Sam0x90 <samuel.rossier@outlook.com>
2023-05-19 23:31:06 +0400

    Update sinkhole.py
    Adding extraction of host and user-agent for logging

--- sinkhole.py
+++ sinkhole.py
@@ -6,29 +6,30 @@ import logging
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger("WebServer")
 
-# Handler class for serving landing page
+# Handler class for serving landing page and handling the redirection 
 class WebHandler(http.server.SimpleHTTPRequestHandler):
         def do_GET(self):
-                #Serve the landing page for HTTP requests
+                logger.info("msg=Incoming_HTTP_GET host=%s user-agent=%s" % (self.headers.get("Host"), self.headers.get("User-Agent")))
+                #S erve the landing page for HTTP requests
                 self.send_response(http.HTTPStatus.OK)
                 self.send_header('Content-type', 'text/html')
                 self.end_headers()
-                
+
                 # Open and read HTML file
                 with open('landing_sink.html', 'r') as file:
                         html_content = file.read()
-                
+
                 # Send the HTML content as the response
                 self.wfile.write(html_content.encode('utf-8'))
 
-# Create a socket server for HTTP port
+# Create a socket server with both HTTP and HTTPS support
 http_server = socketserver.TCPServer(('', 80), WebHandler)
 http_server.allow_reuse_address = True
+logger.info("HTTP server started on port 80")
 
-#Start server
+#Start both servers simultaneously
 try:
         http_server.serve_forever()
-        logger.info("HTTP server started on port 80")
 
 except KeyboardInterrupt:
         pass
