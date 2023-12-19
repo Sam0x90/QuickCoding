8dc4931ad6a18bc084bcd9c38a42f99bb7e1fade
Sam0x90 <samuel.rossier@outlook.com>
2023-12-19 15:32:17 +0100

    Update simple_200_http.py
    Updated to support GET and PUT methods

--- HTTP_Server/simple_200_http.py
+++ HTTP_Server/simple_200_http.py
@@ -4,6 +4,21 @@ import json
 class RequestHandler(BaseHTTPRequestHandler):
 
     def do_POST(self):
+        self.handle_request()
+
+    def do_PUT(self):
+        self.handle_request()
+
+    def do_GET(self):
+        # Send response
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
+
+        # Send the html message
+        self.wfile.write(b"Hello, this is a GET request. Add your response here.")
+
+    def handle_request(self):
         content_length = int(self.headers['Content-Length'])
         post_data = self.rfile.read(content_length)
 
